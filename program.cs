d4139783f3e096269ad0756a8506d06cfaa46ba3
Damian Montero <photoca@hotmail.com>
2022-11-14 15:03:52 -0500

    bank done

--- program.cs
+++ program.cs
@@ -1,1 +1,19 @@
-Console.WriteLine("hello world");
+Console.WriteLine("Welcome to the Bank");
+
+BankAccount account =
+ new BankAccount(11000);
+
+//account.balance = 100;
+try {
+    account.makeDeposit(-1);
+} catch(Exception ex) {
+    Console.WriteLine(ex.Message);
+}
+
+Console.Write($" Your balance is: {account.getBalance()} ");
+Console.WriteLine($" Account Number: {account.accountNumber}");
+
+// public class BankAccount {
+//     public string accountNumber {get; set;} ="";
+//     public decimal balance {get; set;}  = 0;
+// }
